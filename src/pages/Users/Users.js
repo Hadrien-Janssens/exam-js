5e7be2cebf376168e4b93dbf6ef787ea6d005b60
jonathan <wow_Haukins@hotmail.com>
2024-06-03 16:30:56 +0200

    table component

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,13 +1,59 @@
 import { CardsList } from "../../components/CardsList";
+import { DataTable } from "../../components/DataTable";
 import users from "../../storage/users.json";
 import { UserCard } from "./UserCard";
+import { UserRow } from "./UserRow";
 
 export const Users = (element) => {
+  let mode = "grid";
+
   element.innerHTML = `
-    <h1>Utilisateurs</h1>
+    <div class="d-flex justify-content-between">
+      <h1>Utilisateurs</h1>
+      <div>
+        <button id="grid-mode-btn" class="btn btn-sm btn-secondary mr-3 active">
+          <i class="ri-layout-grid-line"></i>
+        </button>
+        <button id="table-mode-btn"  class="btn btn-sm btn-secondary mr-3">
+          <i class="ri-table-line"></i>
+        </button>
+      </div>
+    </div>
     <div id="users-list"></div>
     `;
 
   const usersList = element.querySelector("#users-list");
-  CardsList(usersList, users, UserCard, ["name", "email"]);
+
+  const render = () => {
+    if (mode === "grid") {
+      CardsList(usersList, users, UserCard, ["name", "email"]);
+    } else if (mode === "table") {
+      DataTable(
+        usersList,
+        users,
+        UserRow,
+        ["name", "email"],
+        ["Nom", "Email", "Rôle", "Actions"]
+      );
+    }
+  };
+
+  render();
+
+  const gridModeBtn = document.querySelector("#grid-mode-btn");
+  const tableModeBtn = document.querySelector("#table-mode-btn");
+
+  gridModeBtn.addEventListener("click", () => {
+    mode = "grid";
+    tableModeBtn.classList.remove("active");
+    gridModeBtn.classList.add("active");
+    render();
+  });
+
+  tableModeBtn.addEventListener("click", () => {
+    mode = "table";
+    gridModeBtn.classList.remove("active");
+    tableModeBtn.classList.add("active");
+    render();
+  });
 };
